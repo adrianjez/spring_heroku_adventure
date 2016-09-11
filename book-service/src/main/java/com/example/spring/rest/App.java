f45e90d76d40a0a3f7513132a2f0b76eb72062cb
Adrian <adrian98985@gmail.com>
2016-09-11 15:37:28 +0200

    book_service_init

--- book-service/src/main/java/com/example/spring/rest/App.java
+++ book-service/src/main/java/com/example/spring/rest/App.java
@@ -1,13 +1,18 @@
 package com.example.spring.rest;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+@EnableScheduling
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class);
     }
+    
+    
 }
---
+++ book-service/src/main/java/com/example/spring/rest/repositories/BookRepository.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.spring.rest.repositories;
+
+import com.example.spring.rest.domain.Book;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+
+/**
+ *
+ * @author adrian
+ */
+
+public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+    
+    public Book findByIsbn(@Param("isbn") String isbn);
+    
+    @RestResource(path="getBooksByAuthor")
+    public List<Book> findByAuthorIgnoreCaseOrderByTitleAsc(@Param("author") String author);  
+    
+    @Query("SELECT b FROM Book b WHERE b.published BETWEEN :startYear AND :endYear ORDER BY b.published")
+    public List<Book> getBooksBetweenYears(@Param("startYear")int startYear, @Param("endYear")int endYear);
+    
+    public List<Book> findByTitleContaining(@Param("title") String title); 
+   
+    public int countByAuthor(@Param("author") String author);
+} 
